d89fd0c9fd058737b271c0421b0a8c570d71e981
Blaise NINDENKIMANA <67824289+blaise98-dev@users.noreply.github.com>
2021-12-23 16:39:00 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,15 @@
 <?php
+/*Local connection
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 define('DB_PASS' ,'');
-define('DB_NAME','isuranyayo');
+define('DB_NAME','isuranyayo');*/
+
+//Remote connection
+define('DB_SERVER','bx0patvgx3ikmahu7ich-mysql.services.clever-cloud.com');
+define('DB_USER','uo9xjjfpm8tdrnbl');
+define('DB_PASS' ,'uo9xjjfpm8tdrnbl');
+define('DB_NAME','bx0patvgx3ikmahu7ich');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
